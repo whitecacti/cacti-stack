58170707cc49f432cc8ff86221c708d3980586dd
default <C704839B-921B-45A6-AA05-5644898FAD35@no.no>
2025-12-12 02:10:44 +0000

    cacti stack

---
+++ lib/cacti-stack-stack.ts
@@ -0,0 +1,51 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+export class CactiStackStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const myFunctionNode = new lambda.Function(this, "HelloWorldFunctionNode", {
+      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World! this has been updated and again'),
+          };
+        };
+      `),
+    });
+
+    const myFunctionPython = new lambda.Function(this, "HelloWorldFunctionPython", {
+      runtime: lambda.Runtime.PYTHON_3_12, // or PYTHON_3_11, PYTHON_3_10
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+import json
+
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello World! this has been updated and again')
+    }
+      `),
+        });
+
+    const myFunctionNodeUrl = myFunctionNode.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+    const myFunctionPythonUrl = myFunctionPython.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new cdk.CfnOutput(this, "myFunctionNodeUrlOutput", {
+      value: myFunctionNodeUrl.url,
+    })
+
+    new cdk.CfnOutput(this, "myFunctionPythonUrlOutput", {
+      value: myFunctionPythonUrl.url,
+    })
+  }
+}
---
+++ test/cacti-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CactiStack from '../lib/cacti-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cacti-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CactiStack.CactiStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
